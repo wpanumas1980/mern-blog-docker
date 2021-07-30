1e875836f43ce34a6cb9abda707ced1a7ccb676e
panumas <wpanumas@gmail.com>
2021-07-30 08:16:16 +0700

    next for user password hash

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-
+const authRoute = require('./routes/auth')
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth",authRoute);
 
 mongoose
   .connect(process.env.CONNECTION_URL, { 
