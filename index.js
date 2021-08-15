ae9aea9547a73ae2b448de2d711ecd0b023e2d96
wpanumas1980 <wpanumas@gmail.com>
2021-08-15 16:34:51 +0700

    catergory

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
+const categoryRoute = require('./routes/categories');
+
 const app = express();
 
 app.use(express.json());
@@ -11,6 +13,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 mongoose
   .connect(process.env.CONNECTION_URL, {
