2f413830581aa34cc22b0d6e93c6b4d44131b07b
wpanumas1980 <wpanumas@gmail.com>
2021-08-15 17:12:15 +0700

    upload

--- index.js
+++ index.js
@@ -5,11 +5,25 @@ const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
 const categoryRoute = require('./routes/categories');
+const multer = require('multer');
 
 const app = express();
 
 app.use(express.json());
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "01.jpg");
+  }
+})
+
+const upload = multer({storage:storage});
+app.post("/api/upload", upload.single('file'),(req,res)=>{
+  res.status(200).json("File has been uploaded");
+})
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
